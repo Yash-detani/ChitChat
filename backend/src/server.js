1ee3e76401abdcebab35b99186deeb7033547d7a
Yash Detani <yuvrajmages@gmail.com>
2025-12-20 17:30:39 +0530

    update

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,16 +16,16 @@ dotenv.config();
 
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     origin: "http://localhost:5173",
-    credentials: true
+    credentials: true,
 }
-))
+));
 
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
@@ -34,7 +34,7 @@ if(process.env.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname,"../frontend/dist")));
 
     app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,"../frontend/dist/index.html"));
+        res.sendFile(path.join(__dirname,"../frontend/dist/index.html"));
     })
 }
 
